dd24d66198b60874c4655c7211d983f9a67f0f6c
domenicocandido <candidodomenico87@gmail.com>
2025-03-05 09:49:53 +0100

    Diagrammi_upgrade

--- Lezione_01/Conversione_diagrammi/Diagramma_10.py
+++ Lezione_01/Conversione_diagrammi/Diagramma_10.py
@@ -4,4 +4,4 @@ m:float = float(input("Inserire il valore della media dei voti: "))
 if r < 20000 and m > 27:
     print("Borsa di studio approvata.")
 else:
-    print("Borsa di studiop rifiutata. (Motivo: media o reddito insufficiente)")
+    print("Borsa di studio rifiutata. (Motivo: media o reddito insufficiente)")
