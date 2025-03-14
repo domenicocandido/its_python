65634f54277debf6e4e55c40a5ebf30672363cca
domenicocandido <candidodomenico87@gmail.com>
2025-03-14 19:21:08 +0100

    upgrade

---
+++ ex_2.py
@@ -0,0 +1,15 @@
+class Student:
+     def __init__ (self, name:str, studyProgram:str, age.int, gender:str):
+     
+     self.name = name
+    self.studyProgram = studyProgram
+        self.age = age
+        self.gender = gender
+
+     def printInfo(self):
+         print(f"{}")
+         
+
+sergio = Student("Sergio","Garbatella")
+niccolo = Student("Niccolò", "Firenze")
+domenico = Student("Domenico", "Caserta")
