328c3781beaad011f607306b5cb8e6ac7730f30d
domenicocandido <candidodomenico87@gmail.com>
2025-03-24 17:52:02 +0100

    upgrade

---
+++ Lezione_6/ex_3.py
@@ -0,0 +1,11 @@
+class Animal:
+
+    def __init__(self, name:str, legs:int = 0):
+
+        self.name = name
+        self.legs = legs
+
+    def setLegs(self, legs:int):
+        self.legs = legs
+    
+    def getLegs(self.get_legs)
