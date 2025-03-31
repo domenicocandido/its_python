034df98a71a68b0697cfd3550a7ff73a0ac0afbb
domenicocandido <candidodomenico87@gmail.com>
2025-03-31 13:57:15 +0200

    upgrade

---
+++ Lezione_05/tartaruga_e_lepre.py
@@ -0,0 +1,13 @@
+import random
+
+
+percorso:list[int] = []
+for i in range(71):
+    percorso.append("_")
+    print(*percorso)
+
+posizione = 0
+posizione_nuova = posizione + random.randint(1,10)
+print(percorso_vecchio)
+percorso_nuovo = percorso[0]
+print(percorso_nuovo)
