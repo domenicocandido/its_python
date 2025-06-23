86f56e0da0bc82e3cf010fe2bdf56d3406ac6b67
domenicocandido <candidodomenico87@gmail.com>
2025-06-23 17:07:27 +0200

    update

---
+++ Recupero e Potenziamento/prova.py
@@ -0,0 +1,29 @@
+from random import *
+
+print("*****************")
+print("*               *")
+print("*    capiamo    *")
+print("*               *")
+print("*****************")
+
+def output(mostro):
+
+    larghezza = len(mostro) + 8
+
+    bordo = "*" * larghezza
+
+    vuota = "*" + (" " * (larghezza - 2) ) + "*"
+
+    centrale = "*   " + mostro + "   *"
+
+    return "\n".join([bordo, vuota, centrale, vuota, bordo])
+
+prova = output("Alieno: Robot-25855 ")
+print(prova)
+
+
+setEsempio = {randint(1,100) for x in range(1,16)}
+print(setEsempio)
+print(len(setEsempio))
+
+
